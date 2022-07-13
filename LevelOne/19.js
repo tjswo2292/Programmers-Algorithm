0b037ef0d09bdb0b0af68eea7bc64f396d49045f
tjswo2292 <tjswo2292@naver.com>
2022-07-13 15:33:25 +0900

    다시 풀기

--- LevelOne/19.js
+++ LevelOne/19.js
@@ -11,3 +11,8 @@ function solution(participant, completion) {
 }
 
 console.log(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]));
+
+// 완주하지 못한 선수
+// 효율성 테스트 통과 못함
+// test case 2,5 통과 못함
+// 다시 풀어야 됨

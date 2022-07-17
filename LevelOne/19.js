2a22febf64f509aaf2b2d9757890e14acb56e726
tjswo2292 <tjswo2292@naver.com>
2022-07-17 20:20:19 +0900

    완주하지 못한 선수 구글참고해서 다시 품

--- LevelOne/19.js
+++ LevelOne/19.js
@@ -1,16 +1,14 @@
 function solution(participant, completion) {
-    let array = [...new Set(participant)];
-    array.sort();
-    completion.sort();
-    
-    for(let item of array) {
-        if(!(completion.includes(item))){
-            return item
-        }
+  participant.sort();
+  completion.sort();
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
     }
+  }
 }
 
-console.log(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]));
+console.log(solution(['marina', 'josipa', 'nikola', 'vinko', 'filipa'], ['josipa', 'filipa', 'marina', 'nikola']));
 
 // 완주하지 못한 선수
 // 효율성 테스트 통과 못함

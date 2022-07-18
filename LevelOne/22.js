43662c63534d8a29dd872851f172fcebd71df075
tjswo2292 <tjswo2292@naver.com>
2022-07-18 15:07:32 +0900

    내적

---
+++ LevelOne/22.js
@@ -0,0 +1,12 @@
+// 내적
+
+function solution(a, b) {
+  let result = 0;
+  for (let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
+  }
+
+  return result;
+}
+
+console.log(solution([1, 2, 3, 4], [-3, -1, 0, 2]));

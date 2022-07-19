85b6971f63288157290ef3597e767c011044f46d
tjswo2292 <tjswo2292@naver.com>
2022-07-19 23:42:25 +0900

    카카오 키패드 푸는 중

--- LevelOne/21.js
+++ LevelOne/21.js
@@ -1,30 +1,6 @@
 // 크레인 인형뽑기
 
 function solution(board, moves) {
-  const basket = [];
-  let result = 0;
-  moves.forEach((order) => {
-    const doll = pickup(board, order - 1);
-    if (doll) {
-      if (basket[basket.length - 1] === doll) {
-        basket.pop();
-        result += 2;
-      } else {
-        basket.push(doll);
-      }
-    }
-  });
-  return result;
-}
-
-function pickup(board, order) {
-  for (let i = 0; i < board.length; i++) {
-    if (board[i][order] !== 0) {
-      const doll = board[i][order];
-      board[i][order] = 0;
-      return doll;
-    }
-  }
 }
 
 console.log(solution(
@@ -34,5 +10,5 @@ console.log(solution(
     [0, 2, 5, 0, 1],
     [4, 2, 4, 4, 2],
     [3, 5, 1, 3, 1]],
-  [1, 5, 3, 5, 1, 2, 1, 4],
+  [1, 5, 3, 5, 1, 2, 1, 4], // 뽑을 순서
 ));
---
+++ LevelOne/25.js
@@ -0,0 +1,12 @@
+function solution(numbers, hand) {
+
+}
+
+console.log(solution(
+  [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5],
+  'right',
+));
+
+// 순서대로 누를 번호가 담긴 배열 numbers
+// 왼,오 잡이 나타내는 문자열 hand
+// 각 번호를 누른 엄지손가락이 왼손인 이 오른손인 지를 나타내는 연속된 문자열 return

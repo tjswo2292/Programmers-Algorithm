a457c8031ee4d10a4557e0232143e205adac1db9
tjswo2292 <tjswo2292@naver.com>
2022-07-29 21:33:14 +0900

    다리르 지나는 트럭 ing

---
+++ LevelOne/30.js
@@ -0,0 +1,29 @@
+function solution(bridge_length, weight, truck_weights) {
+    // 하나의 동작이 끝나면 시간을 +1 해준다.
+    // truck_weights의 length가 0이면 time을 return한다. -> 대기 트럭이 없어도 다리를 건너는 중인 트럭이 있을 수도 있으니 return 값에 +1을 해준다.
+
+    let time = 0; // 시간
+    let pending = []; // 대기 중인 트럭
+    let maxWeight = 0;
+
+    while(truck_weights !== 0) {
+        // 트럭 대수와 무게를 조건으로 걸어야 함
+        maxWeight = pending.forEach( element => maxWeight += element );
+        if(pending.length <= bridge_length && maxWeight <= weight) {
+            pending = truck_weights.shift();
+            time++;
+        } else {
+            pending.shift();
+            time++;
+        }
+    }
+    
+    return time + 1;
+}
+
+console.log(solution(2, 10, [7, 4, 5, 6]));
+
+// bridge_length : 다리에 올라 갈  수 있는 트럭의 수
+// weight : 다리가 견딜 수 있는 무게
+// truck_weights : 트럭 별 무게
+// 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 return
